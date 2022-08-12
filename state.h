77c480bcb029f64876294b746f7226953da50f1f
王晓阳 <wangxy555@mail2.sysu.edu.cn>
2022-08-12 18:21:08 +0800

    Update state.h

--- state.h
+++ state.h
@@ -19,12 +19,12 @@ class state
 private:
 	short length, snake[(M - 2) * (N - 2)][2];
 	char direction, food[2];
-	void food_restart();
+	void food_restart();//重新生成食物，不与蛇身重合
 public:
 	state();
-	friend int remove(state*, char);
-	friend int judge(state*);
-	friend void putout_snake(state*, int);
-	friend void level_mode(state*);
-	friend void endless_mode(state*);
+	friend int remove(state*, char);//蛇移动
+	friend int judge(state*);//判断蛇是否越界或者吃到自己
+	friend void putout_snake(state*, int);//蛇移动后重新绘制蛇
+	friend void level_mode(state*);//关卡模式，分数有上限
+	friend void endless_mode(state*);//无尽模式，分数无上限
 };
